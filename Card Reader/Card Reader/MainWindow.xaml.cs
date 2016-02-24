875f0465406417f9d050ff18b0612e55b6776d86
Dante.Nardo <dcn5783@g.rit.edu>
2016-02-24 15:07:17 -0500

    Overview: Added file selection functionality
    Detailed: I added three functions. One to open files, one to open folders, and one to select files in the file list. No true input or output yet, only file selection.

--- Card Reader/Card Reader/MainWindow.xaml.cs
+++ Card Reader/Card Reader/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Card_Reader
 {
@@ -23,6 +24,55 @@ namespace Card_Reader
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		// Select a file and open it
+		private void OpenFile_Click(object sender, RoutedEventArgs e)
+		{
+			// Create OpenFileDialog 
+			Microsoft.Win32.OpenFileDialog fibox = new Microsoft.Win32.OpenFileDialog();
+
+			// Set filter for file extension and default file extension 
+			fibox.DefaultExt = ".xml";
+			fibox.Filter = "XML Files (*.xml)|*.xml";
+
+			// Show open file dialog box
+			Nullable<bool> result = fibox.ShowDialog();
+
+			// Process open file dialog box results
+			if (result == true)
+			{
+				// Open document
+				CurrentFile.Text = fibox.FileName;
+			}
+		}
+
+		// Select a folder and load its contents into the file list
+		private void OpenFolder_Click(object sender, RoutedEventArgs e)
+		{
+			// Create FolderBrowserDialog
+			System.Windows.Forms.FolderBrowserDialog fobox = new System.Windows.Forms.FolderBrowserDialog();
+
+			// Save folder files
+			System.Windows.Forms.DialogResult result = fobox.ShowDialog();
+
+			// If they did not cancel or close the form
+			if (result != System.Windows.Forms.DialogResult.Cancel)
+			{
+				// Sets the FileList to display the files
+				FileList.ItemsSource = Directory.GetFiles(fobox.SelectedPath);
+			}
+		}
+
+		// Selects a file from the FileList when it is double clicked on
+		private void FileList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			// If they double clicked with the left mouse button
+			if (e.LeftButton == MouseButtonState.Pressed)
+			{
+				// Sets the CurrentFile's text to equal the current selected file
+				CurrentFile.Text = (string)FileList.Items[FileList.SelectedIndex];
+			}
 		}
 	}
 }
